f414c76440798a6ac5e4c5e2551c2c25040f859f
Tibi Kolozsi <tibi@shapr3d.com>
2022-02-01 15:12:10 +0100

    merge_back

---
+++ .github/scripts/merge_back.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+REPO_ROOT=$(cd "$(dirname "$0")/../.."; pwd)
+
+set -euo pipefail
+IFS=$'\n\t'
+
+base_branch="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$base_branch" != "master" ]]; then
+  echo 'Aborting script, base branch should be master';
+  exit 1;
+fi
+merge_branch="merge_release"
+release_branch="release"
+
+git checkout -b "$merge_branch"
+git merge "origin/$release_branch"
+
+# if there are conflicts
+if [[ "$(git status --porcelain --ignore-submodules)" != "" ]]; then
+    git add .
+    git merge --continue
+fi
+
+git push -f origin HEAD:"$merge_branch"
+
+# Open new PR
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" --data "{\
+    \"title\":\"L10n - update\",\
+    \"base\":\"$base_branch\",\
+    \"head\":\"$merge_branch\"\
+}" https://api.github.com/repos/tibiiii/playground/pulls > /dev/null
