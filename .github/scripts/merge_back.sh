d7a1e263b6b053a655e60ac5a2de6cbe08eb63e6
Tibi Kolozsi <tibi@shapr3d.com>
2022-02-02 11:43:14 +0100

    author

--- .github/scripts/merge_back.sh
+++ .github/scripts/merge_back.sh
@@ -9,11 +9,16 @@ fi
 merge_branch="merge_release"
 release_branch="release"
 
-if git show-branch "origin/merge_release" &>/dev/null; then
+git config --global user.name "Shapr3D Dev"
+git config --global user.email "dev@shapr3d.com"
+
+if git show-branch "origin/$merge_branch" &>/dev/null; then
     # if a `merge_branch` already exists
     # 1. check if merge without conflict is possible for `master` and `release`
     #    if yes, do the merge and push
     #    if no, abort, merge resolution has to be done manually
+
+    echo "Previous \"origin/$merge_branch\" already exists"
 
     git checkout "$merge_branch"
     git merge --no-commit --no-ff "origin/$base_branch"
@@ -30,16 +35,18 @@ if git show-branch "origin/merge_release" &>/dev/null; then
     elif [[ "$(git status --porcelain --ignore-submodules)" != "" ]]; then
         git commit --all --no-edit
     fi
-    git push "$merge_branch"
+    git push origin HEAD:"$merge_branch"
 else
     # if no `merge_branch`
     # 1. checkout `release`
     # 2. create new `merge_branch` from `release`
     # 3. open a PR `merge_branch` -> `master`
+    echo "No previous \"origin/$merge_branch\""
+
     git checkout "$release_branch"
     git checkout -b "$merge_branch"
     git push -f origin HEAD:"$merge_branch"
-    
+
     # Open new PR
     curl -s -S -H "Authorization: token $GITHUB_TOKEN" --data "{\
         \"title\":\"L10n - update\",\
